7932d6244abc099ddd80a40f61c15c64b635158e
Pazia Ramirez <pazia008@gmail.com>
2021-04-05 11:45:36 -0500

    can delete a tag

--- TabloidMVC/Repositories/ITagRepository.cs
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -9,5 +9,7 @@ namespace TabloidMVC.Repositories
     public interface ITagRepository
     {
         List<Tag> GetAllTags();
+        Tag GetTagById(int id);
+        void DeleteTag(int id);
     }
 }
