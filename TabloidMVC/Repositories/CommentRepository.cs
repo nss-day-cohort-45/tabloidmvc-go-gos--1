eb55c1512a6f86497673a49297aced745dd787b3
Chris-Denmark <cdenmark97@gmail.com>
2021-04-06 11:04:00 -0500

    commiting to merge new main

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -1,11 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using TabloidMVC.Models;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace TabloidMVC.Repositories
 {
-    public class CommentRepository
+    public class CommentRepository : BaseRepository, ICommentRepository
     {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public void Add(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Comment (
+                            PostId, UserProfileId, Subject, Content, CreateDateTime )
+                        OUTPUT INSERTED.ID
+                        Values (
+                            @PostId, @UserProfileId, @Subject, @Context, @CreateDateTime )
+                        ";
+                    cmd.Parameters.AddWithValue("@PostId", comment.PostId);
+                    cmd.Parameters.AddWithValue("@UserProfileId", comment.UserProfileId);
+                    cmd.Parameters.AddWithValue("@Subject", comment.Subject);
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", comment.CreateDateTime);
+
+                    comment.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public List<Comment> GetCommentsByPost(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id, c.PostId, c.UserProfileId, 
+                               c.Subject, c.Content, c.CreateDateTime,
+                               u.DisplayName AS userName
+                        FROM Comment c
+                        LEFT JOIN UserProfile u ON c.UserProfileId = u.Id
+                        WHERE @postId = c.PostId
+                        ORDER BY CreateDateTime DESC
+                     ";
+
+                    cmd.Parameters.AddWithValue("@postId", postId);
+
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(NewCommentFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return comments;
+
+                }
+            }
+        }
+
+
     }
 }
--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Repositories
 {
-    public class ICommentRepository
+    public interface ICommentRepository
     {
-        public interface ICommentRepository
-        {
-
-        }
+        void add(Comment comment);
+        void delete(int id);
+        void UpdateComment(Comment comment);
+        List<Comment> GetCommentsByPost(int postId);
+        Comment getCommentById(int id);
     }
 }
